a00489a0ab91b982ada2fcc4ae60ad4522d74705
ZeroQI <benjamin.brisson@gmail.com>
2013-09-04 17:34:22 +0100

    Create 06..Curriculum Vitae.php

---
+++ pages/01.Public/06..Curriculum Vitae.php
@@ -0,0 +1,4 @@
+<?php
+article("Curriculum Vitae (<a href='download/text/cv/CV_English.zip'>doc</a> 9KB or <a href='download/text/cv/CV_English_rtf.zip'>rtf</a> 3KB or <a href='download/text/cv/CV_english.html'>html</a> 6KB)", "title", "content", "*", "*");
+include "download/text/cv/CV_English.html";
+?>
