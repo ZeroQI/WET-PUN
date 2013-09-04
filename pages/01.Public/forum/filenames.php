459542b176d2d3b37a81e7086c5d07af755ca000
ZeroQI <benjamin.brisson@gmail.com>
2013-09-04 17:35:20 +0100

    Create filenames.php

---
+++ pages/01.Public/forum/filenames.php
@@ -0,0 +1,21 @@
+<?php
+
+//Root folder html templates, no php codes, variables used: %var%
+$TPL_INDEX   = "template_index.htm";   //Variables: %CSS-FILE% %PHP_POST%   %RSS-FILE%  -  %ICON% %TITLE% %AUTHOR% %PUBDATE% %CATEGORY% %LINK%
+$TPL_THREADS = "template_threads.htm"; //Variables: %CSS-FILE% %PHP_POST%   %RSS-FILE%  -  %ICON% %TITLE% %AUTHOR% %PUBDATE% %CATEGORY% %LINK%
+$TPL_POST    = "template_post.htm";    //Variables: %CSS-FILE% %PHP_ENGINE% %OBJECT%
+$TPL_CSS     = "template.css";
+
+//Root folder 
+$PHP_INDEX   = "index.php";
+$PHP_THREADS = "threads.php";
+$PHP_POST    = "post.php";
+$PHP_ENGINE  = "update.php";
+
+//Board folder
+$HTM_INDEX   = "board.htm";
+$RSS_INDEX   = "board.rss";
+
+$DATE_FORMAT = "Y-m-d D, H\hi";
+
+?>
